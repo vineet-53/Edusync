d1e43b2dfc7c83981c3bca8a24e0e41656c0ce83
vineet-53 <vineetsinghnegi53@gmail.com>
2025-12-30 13:18:12 +0530

    minor changes

--- backend/utils/imageUploader.js
+++ backend/utils/imageUploader.js
@@ -5,8 +5,8 @@ exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
     const options = {folder};
 
     if(height) options.height = height;
-    if(quality) options.quality = quality;                                 // height and quality is used to image compression
-    options.resource_type = "auto";                                       // it auto fetch image type;
+    if(quality) options.quality = quality; 
+    options.resource_type = "auto";   
 
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }   
